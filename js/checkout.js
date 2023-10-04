49dbe37b1f6e1b1b592796444974ea7ce0ab107e
Muhammad Ramadhan Muna <ramamuna08@gmail.com>
2023-10-04 13:06:56 +0700

    selesai tahap 2

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,4 @@
+// Modal Change Address
 // Get references to the modal and buttons
 const modal = document.getElementById("modal");
 const openModalBtn = document.getElementById("openModalBtn");
@@ -17,5 +18,66 @@ closeModalBtn.addEventListener("click", () => {
 window.addEventListener("click", (event) => {
   if (event.target === modal) {
     modal.style.display = "none";
+  }
+});
+
+// Modal Transfer
+// Get references to the modal and buttons
+const modalTransfer = document.getElementById("modalTransfer");
+const openTransferBtn = document.getElementById("button-transfer");
+const closeTransferBtn = document.getElementById("closeTransferBtn");
+
+// Open the modal when the "Open Modal" button is clicked
+openTransferBtn.addEventListener("click", () => {
+  modalTransfer.style.display = "block";
+});
+
+// Close the modal when the "Close" button is clicked
+closeTransferBtn.addEventListener("click", () => {
+  modalTransfer.style.display = "none";
+});
+
+// Close the modal if the user clicks outside of it
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modalTransfer.style.display = "none";
+  }
+});
+
+// Button
+// Get a reference to the button element
+const button1 = document.getElementById("button-cod");
+const button2 = document.getElementById("button-transfer");
+
+// Add a click event listener to the button
+button1.addEventListener("click", function () {
+  // Toggle the 'active' class on the button element
+  this.classList.toggle("active");
+});
+
+// Add a click event listener to the button
+button2.addEventListener("click", function () {
+  // Toggle the 'active' class on the button element
+  this.classList.toggle("active");
+});
+
+// View Image Upload
+// Get references to the input and image elements
+const fileInput = document.getElementById("fileInput");
+const previewImage = document.getElementById("previewImage");
+
+// Add an event listener to the file input
+fileInput.addEventListener("change", function () {
+  const selectedFile = this.files[0];
+  if (selectedFile) {
+    const reader = new FileReader();
+
+    reader.onload = function (event) {
+      previewImage.src = event.target.result;
+    };
+
+    reader.readAsDataURL(selectedFile);
+  } else {
+    previewImage.src = "";
   }
 });
